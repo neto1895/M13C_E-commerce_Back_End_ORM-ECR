a791c89f54ba930908da2225780a35109a1d158a
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-06-13 04:29:30 -0600

    tag lines updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,10 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product}]})
+      include: [
+        { model: Product}
+      ]
+    });
       res.status(200).json(tagData);
     } catch (err){
       res.status(500).json(err);
