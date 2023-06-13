e7d3f181e56a9c19e458648a2896e993311f8e82
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-06-13 00:22:29 -0600

    create the database models

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: [10, 2], // 10 digits maximum, 2 decimal places maximum
+          msg: 'Price must be a valid decimal with a maximum of 10 digits and 2 decimal places.',
+        },
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, // Doesn't allow null values
+      defaultValue: 10, // Set a default value of 10
+      validate: {
+        isNumeric: {
+          msg: 'Stock must be a numeric value.', // Validates that the value is numeric
+        },
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category', // References the category model's id
+        key: 'id',
+      },
+    }
+
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -6,26 +6,31 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  // through: {
+  //   model: Tag,
+  //   unique: false
+  // },
+  // // Define an alias for when data is retrieved
+  // as: 'products_test'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+  foreignKey: 'category_name',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id',
+  foreignKey: 'product_name',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id',
+  foreignKey: 'tag_name',
 });
-
 
 module.exports = {
   Product,
