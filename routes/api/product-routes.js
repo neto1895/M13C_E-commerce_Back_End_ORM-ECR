473eea58af685c806abc75e32a9f7adb092f38a7
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-06-13 03:26:59 -0600

    product routes updated with put and delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,8 +25,11 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Category.findByPk(req.params.id, {
-      include: [{model: Product}]
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category}, 
+        { model: Tag}
+      ]
     });
     if (!productData) {
       res.status(404).json({ message: 'No location found with this id!' });
@@ -113,8 +116,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json({ message: 'Product deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
